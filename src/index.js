01ee4e0bede63d247ec8f9f3dcd28d9ee411b682
Mariia Hunko <masha.gunko96@gmail.com>
2021-04-06 22:23:49 +0300

    Пофиксила index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import './sass/main.scss'
-import headerMain from './templates/header-main.hbs'
-import headerLibrary from './templates/header-library.hbs'
-import movieGallery from './templates/movie-gallery.hbs'
-import spiner from './templates/spiner.hbs'
-import footer from './templates/footer.hbs'
-import modalWindow from './templates/modal-window.hbs'
-import footerSrc from './images/sprite.svg'
-console.log(footerSrc)
+import './sass/main.scss';
+import headerMain from './templates/header-main.hbs';
+import headerLibrary from './templates/header-library.hbs';
+import movieGallery from './templates/movie-gallery.hbs';
+import spiner from './templates/spiner.hbs';
+import footer from './templates/footer.hbs';
+import modalWindow from './templates/modal-window.hbs';
+import footerSrc from './images/sprite.svg';
+
 //-----------------------------------------------------------
-import './js/renderMarkupMovie.js'
+import './js/renderMarkupMovie.js';
 const refs = {
     headerMain: document.getElementById('header-main'),
     headerLibrary: document.getElementById('header-library'),
@@ -22,7 +22,7 @@ refs.headerMain.innerHTML = headerMain();
 refs.headerLibrary.innerHTML = headerLibrary();
 refs.gallery.innerHTML = movieGallery();
 refs.spiner.innerHTML = spiner();
-refs.footer.innerHTML = footer();
+refs.footer.innerHTML = footer({src: footerSrc});
 refs.modalWindow.innerHTML = modalWindow();
 
 
