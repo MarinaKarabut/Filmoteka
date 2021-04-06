6528627e7aeb296a985a7837e93bfa3a4130d66e
Maksim Shipovskiy <m.shipovskiy@gmail.com>
2021-04-06 15:49:42 +0400

    made footer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import headerMain from './templates/header-main.hbs';
-import headerLibrary from './templates/header-library.hbs';
-import movieGallery from './templates/movie-gallery.hbs';
-import spiner from './templates/spiner.hbs';
-import footer from './templates/footer.hbs';
-import modalWindow from './templates/modal-window.hbs';
-
+import headerMain from './templates/header-main.hbs'
+import headerLibrary from './templates/header-library.hbs'
+import movieGallery from './templates/movie-gallery.hbs'
+import spiner from './templates/spiner.hbs'
+import footer from './templates/footer.hbs'
+import modalWindow from './templates/modal-window.hbs'
+import footerSrc from './images/sprite.svg'
 const refs = {
-    headerMain: document.getElementById('header-main'),
-    headerLibrary: document.getElementById('header-library'),
-    gallery: document.getElementById('movie-gallery'),
-    spiner: document.getElementById('spiner-container'),
-    footer: document.getElementById('footer'),
-    modalWindow: document.getElementById('modal-window'),
-};
+  headerMain: document.getElementById('header-main'),
+  headerLibrary: document.getElementById('header-library'),
+  gallery: document.getElementById('movie-gallery'),
+  spiner: document.getElementById('spiner-container'),
+  footer: document.getElementById('footer'),
+  modalWindow: document.getElementById('modal-window'),
+}
 
-refs.headerMain.innerHTML = headerMain();
-refs.headerLibrary.innerHTML = headerLibrary();
-refs.gallery.innerHTML = movieGallery();
-refs.spiner.innerHTML = spiner();
-refs.footer.innerHTML = footer();
-refs.modalWindow.innerHTML = modalWindow();
+refs.headerMain.innerHTML = headerMain()
+refs.headerLibrary.innerHTML = headerLibrary()
+refs.gallery.innerHTML = movieGallery()
+refs.spiner.innerHTML = spiner()
+refs.footer.innerHTML = footer({src: footerSrc})
+
+refs.modalWindow.innerHTML = modalWindow()
