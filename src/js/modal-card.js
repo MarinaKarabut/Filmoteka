a40ea2a0b89971a448cef43ef43055e8b679b7ce
Maksim Shipovskiy <m.shipovskiy@gmail.com>
2021-04-07 14:19:55 +0400

    s

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -1,5 +1,31 @@
 import refs from './refs.js'
 import modalCardRender from '../templates/cardsOpenModalMovies.hbs'
+function getFilmById(id) {
+  const finalAddress = `${BASE_URL}/${id}?api_key=${BASE_API}`
+  const idReq = fetch(finalAddress)
+  return idReq
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Запрос не удался')
+      }
+      const goodresponse = response.json()
+      return goodresponse
+    })
+    .then(({original_title, overview, poster_path, vote_average, vote_count, popularity, genres}) => ({
+      original_title,
+      overview,
+      poster_path,
+      vote_average,
+      vote_count,
+      popularity,
+      genres,
+    }))
+}
+
+const BASE_API = '923c2cf88ec4338da74c768a045101f0'
+const BASE_URL = 'https://api.themoviedb.org/3/trending/movie'
+
+console.log(getFilmById(46))
 
 refs.gallery.addEventListener('click', onClickGallery)
 
@@ -10,4 +36,10 @@ function onClickGallery(e) {
   }
 
   refs.modal.classList.add('is-open')
+  const id = refs.imgCard.dataset.index
+  const dataSource = getFilmById(id)
+
+  dataSource.then(result => {
+    console.log(result)
+  })
 }
