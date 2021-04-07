a1d9f0fecbbc72180b40548e49f414ca71b1053f
Sasha <protzenko.as@gmail.com>
2021-04-07 14:46:33 +0300

    add change

---
+++ src/js/toggleBtnInputHeader.js
@@ -0,0 +1,11 @@
+import refs from './refs.js'
+
+refs.btnWatchedHeader.addEventListener('click', onToggleBtnInputHeader);
+
+function onToggleBtnInputHeader() {
+    
+    if (!btnWatchedHeader) {
+        
+    }
+
+}
