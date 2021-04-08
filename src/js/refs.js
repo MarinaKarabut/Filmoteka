09efce9ff3a10a17e8bbdb023675a932f2662c64
Mariia Hunko <masha.gunko96@gmail.com>
2021-04-08 15:38:17 +0300

    refs.js was deleted

--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-export default {
-    gallery: document.querySelector('.js-gallery-movies'),
-    searchForm: document.querySelector('.form-search'),
-    modal: document.querySelector('.js-modal'),
-
-    btnWatchedHeader: document.getElementById('js-btn-watched'),
-    btnQueueHeader: document.getElementById('js-btn-queue')
-};
