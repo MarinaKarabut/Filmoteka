5f7a50d67c7f1a3db0c0221f6fea0d5156a48ae0
fahriddinnomanjanov <58633040+fahriddinnomanjanov@users.noreply.github.com>
2021-04-10 11:31:09 +0300

    fix

--- src/js/addToQueue.js
+++ src/js/addToQueue.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-function addToQueue() {
-    const addedFilmsId = [];
-    const imgCard = document.querySelector('.film-info__img');
-    const { filmId } = imgCard.dataset;
-    addedFilmsId.push(filmId);
-    const filmIdStr = JSON.stringify(addedFilmsId);
-    localStorage.setItem('id', filmIdStr);
-=======
 function addToQueue(e) {
   const addedFilmsId = []
   const imgCard = document.querySelector('.film-info__img')
@@ -14,7 +5,6 @@ function addToQueue(e) {
   addedFilmsId.push(filmId)
   const filmIdStr = JSON.stringify(addedFilmsId)
   localStorage.setItem('id', filmIdStr)
->>>>>>> dev
 }
 
-export default addToQueue
+export default addToQueue;
