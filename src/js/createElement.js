6764a536af635f88d72b693c254d95e6856062ca
Mariia Hunko <masha.gunko96@gmail.com>
2021-04-12 08:45:14 +0300

    Коммит последних изменений

--- src/js/createElement.js
+++ src/js/createElement.js
@@ -1,12 +1,12 @@
-const createElement = ({tag = "div", classList = "", content = "", attrs = []}) => {
+const createElement = ({ tag = "div", classList = "", content = "", attrs = [] }) => {
     const element = document.createElement(tag);
     element.className = classList;
     element.innerHTML = content;
-    attrs.forEach(({name, value}) => {
+    attrs.forEach(({ name, value }) => {
         element[name] = value;
-    })
+    });
     return element;
-}
+};
 
 export default createElement;
 
