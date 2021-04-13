213967d2ed3c9474ed0f31d021ffe5af4fe7b3cd
Akbar <k.akbar1101@gmail.com>
2021-04-13 10:45:10 +0300

    commit

--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -1,5 +1,6 @@
 import filmListTemplate from '../templates/filmList.hbs'
 import createPagination from './createPagination.js'
+import filmsSearchOptions from "./filmOptions.js"
 
 function renderFilms(films, filmsContainer) {
   filmsContainer.innerHTML = filmListTemplate(films.results)
@@ -11,6 +12,19 @@ function renderFilms(films, filmsContainer) {
   }
 
   filmsContainer.after(pagination)
+  
+  const arrowLeft = document.getElementById('arrow-left')
+  const arrowRight = document.getElementById('arrow-right')
+     if (filmsSearchOptions.options.page === 1) {
+        arrowLeft.setAttribute('disabled', 'disabled')
+    }else{
+      arrowLeft.removeAttribute('disabled')
+  }
+  if (filmsSearchOptions.options.page === films.total_pages) {
+    arrowRight.setAttribute('disabled', 'disabled')
+  } else {
+    arrowRight.removeAttribute('disabled')
+  }
 }
 
 export default renderFilms
